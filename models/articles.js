853b27658ec75f94b3e3e2c509d1f48565c0e6fc
Jamal Whorms-MonDesir <jamal@letswayfare.com>
2022-08-03 20:36:41 +0100

    Refactor fetchArticleByID to include comment count with matching test

--- models/articles.js
+++ models/articles.js
@@ -2,7 +2,16 @@ const db = require('../db/connection');
 
 exports.fetchArticleByID = (id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1;`, [id])
+    .query(
+      `SELECT 
+      articles.*, COUNT(comments.comment_id)::int AS comment_count
+    FROM articles
+    FULL OUTER JOIN comments 
+    ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id;`,
+      [id]
+    )
     .then((article) => {
       if (article.rows.length < 1) {
         return Promise.reject({ status: 404, msg: 'Article not found' });
