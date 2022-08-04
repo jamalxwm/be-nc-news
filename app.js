cadea97aad67479ef16a8bff7bdd0f56f47cca2f
Jamal Whorms-MonDesir <jamal@letswayfare.com>
2022-08-04 11:40:45 +0100

    Add error handling test for invalid syntax on patchArticleById

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const { getTopics } = require('./controllers/topics');
 const { getArticlesByID, patchArticleByID } = require('./controllers/articles');
-const { handlePsqlErrors, handleCustomErrors } = require('./errors');
+const errors = require('./errors');
 
 app.use(express.json());
 
@@ -11,8 +11,7 @@ app.get('/api/topics', getTopics);
 app.get('/api/articles/:article_id', getArticlesByID);
 app.patch('/api/articles/:article_id', patchArticleByID);
 
-app.use(handleCustomErrors);
-app.use(handlePsqlErrors);
+app.use(errors);
 
 app.all('/*', (req, res) => {
   res.status(404).send({ msg: 'Not found' });
